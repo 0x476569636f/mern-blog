1364c20aefc9a799ed5a39e9a5f5f95503d19cdf
Irvan <darknet1255@gmail.com>
2023-12-27 19:32:19 +0700

    Konfigurasi react router & layouting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
 import Header from "./components/Header";
 import Post from "./components/Post";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./layout/Layout";
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Post />
-      <Post />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route
+          index
+          element={
+            <>
+              <Post />
+              <Post />
+            </>
+          }
+        />
+        <Route path="/login" element={<div>Login Page</div>} />
+        <Route path="/register" element={<div>Register Page</div>} />
+      </Route>
+    </Routes>
   );
 };
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
@@ -5,8 +7,8 @@ const Header = () => {
         MyBlog
       </a>
       <div className="navigation">
-        <a href="">Login</a>
-        <a href="">Register</a>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
       </div>
     </header>
   );
