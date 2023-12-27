6fdb21c1e64877920128f69075ac6515656930c9
Irvan <darknet1255@gmail.com>
2023-12-27 19:36:03 +0700

    Route to homepage
    when click logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-      <a href="" className="logo">
+      <Link to="" className="logo">
         MyBlog
-      </a>
+      </Link>
       <div className="navigation">
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
